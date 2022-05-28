5bb9159ee4e94737c6df8c8d5258996759267abb
sm-jahangir <samraatjahangir@gmail.com>
2022-05-29 00:01:33 +0600

    axios react-form-hook toastify install

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
