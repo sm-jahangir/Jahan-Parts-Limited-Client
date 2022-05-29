d2d55a3b63e5da7f76fbcee575b828b3cdf04704
sm-jahangir <samraatjahangir@gmail.com>
2022-05-29 16:59:34 +0600

    banner design

--- src/components/Home/HomeSlider.js
+++ src/components/Home/HomeSlider.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const HomeSlider = () => {
   return (
-    <div>
-      <h2>SLider</h2>
+    <div
+      className=""
+      style={{
+        background: "url('slider/slider-1.webp')",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="p-7">
+        <div className="p-10 ml-5">
+          <button className="btn btn-warning btn-xs">Weekend Discount</button>
+          <h1 className="text-4xl font-light">The Features of Helth</h1>
+          <h1 className="text-5xl font-bold">Shopping With Style</h1>
+          <p className="py-6">
+            Big screens in incredibly slim designs in your hand.Big screens in
+            incredibly slim designs in your hand.
+          </p>
+          <button className="btn btn-outline btn-primary">Shop Now</button>
+        </div>
+      </div>
     </div>
   );
 };
